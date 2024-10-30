fd994d82fcc3ab85a716b2c9abe9488fa3698984
Cuadrado1092 <jcuadrado1092@uta.edu.ec>
2024-10-30 08:32:28 -0500

    Agregando el registro

--- src/Pages/Gestion_usuarios.jsx
+++ src/Pages/Gestion_usuarios.jsx
@@ -1,26 +1,35 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from "react";
+import Modal_Ges from "../Pages_Admin/Registro_adm";
 import "../Styles/Gestion_Usuario.css";
 
 const Gestion_usuarios = () => {
+  const [showModal, setShowModal] = useState(false);
+  const handleSessionClick = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
   const [users, setUsers] = useState([]);
   useEffect(() => {
-    fetch('http://localhost/Api-DriverGo/Ver_usuarios.php')
-      .then(response => response.json())
-      .then(data => {
-        if(data.status) {
-          setUsers(data.users); 
+    fetch("http://localhost/Api-DriverGo/Ver_usuarios.php")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.users);
+        if (data.status) {
+          setUsers(data.users);
         } else {
           console.error("Error: ", data.message);
         }
       })
-      .catch(error => console.error('Error fetching users:', error));
+      .catch((error) => console.error("Error fetching users:", error));
   }, []);
   return (
     <div className="card">
       <div className="card-header">
         <h2 className="card-tit">Gestión de Usuarios</h2>
-        <button className="btn-nu">
+        <button className="btn-nu" onClick={handleSessionClick}>
           <i className="fa-solid fa-plus"></i>Nuevo Usuario
         </button>
       </div>
@@ -36,28 +45,28 @@ const Gestion_usuarios = () => {
             </tr>
           </thead>
           <tbody>
-            {users.map((user)=>(
-             <tr key={user.id}>
-              <td className="table-cell">{user.nom_usu}</td>
-              <td className="table-cell">{user.ape_usu}</td>
+            {users.map((user) => (
+              <tr key={user.id_usu}>
+                <td className="table-cell">{user.nom_usu}</td>
+                <td className="table-cell">{user.ape_usu}</td>
                 <td className="table-cell">{user.corr_usu}</td>
                 <td className="table-cell">{user.cargo}</td>
                 <td className="table-cell">
-                  <div className='btn-activs'>
-                    <button className='btn-acti'>
+                  <div className="btn-activs">
+                    <button className="btn-acti">
                       <i className="fa-solid fa-pencil"></i>
                     </button>
-                    <button className='btn-acti'>
+                    <button className="btn-acti">
                       <i className="fa-solid fa-trash"></i>
                     </button>
                   </div>
-                  </td>
-             </tr> 
-        ))}
-
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
+      {showModal && <Modal_Ges closeModal={closeModal} />}
     </div>
   );
 };
