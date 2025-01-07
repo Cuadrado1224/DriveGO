03685e5161efc0d0cb086b4c6261894b09408597
ArteMisa82 <anavero162@gmail.com>
2025-01-06 20:36:48 -0500

    Actualización de lugares

--- src/Pages/Lugares.jsx
+++ src/Pages/Lugares.jsx
@@ -4,20 +4,44 @@ import  "../Styles/Lugares.css";
 const Oficinas =[
   { Ciudad: 'Quito', imagen:'/quito.jpg', 
     direccion: 'Aeropuerto Internacional Mariscal Sucre',
-    Contactos: 'Call center: (593) 22541683'
+    Contactos: 'Call center: (593) 22541683',
+    email: "proyectodrivego@gmail.com",
+    descripcion:  "Localice nuestras oficinas en el aeropuerto de Quito.",
+    horario: {
+        semana: "Lunes-Viernes: 8AM - 4PM",
+        finDeSemana: "Sabado: 9AM - 3PM",
+    },
   },
   { Ciudad: 'Guayaquil', imagen:'/guayaquil.png', 
     direccion: 'Aeropuerto Internacional Jose Joaquin de Olmedo',
-    Contactos: 'Call center: (593) 48620819'
+    Contactos: 'Call center: (593) 48620819',
+    email: "proyectodrivego@gmail.com",
+    descripcion:  "Localice nuestras oficinas afuera del areopuerto de Guayaquil.",
+    horario: {
+      semana: 'Lunes-Viernes: 8AM - 4PM',
+      finDeSemana: "Sabado: 9AM - 3PM",
+    },
   },
-  { Ciudad: 'Cuenca Aeropuerto', imagen:'/cuenca.png', 
+  { Ciudad: 'Cuenca', imagen:'/cuenca.png',
     direccion: 'Aeropuerto Mariscal la Mar',
-    Contactos: 'Call center: (593) 75390635'
+    Contactos: 'Call center: (593) 75390635',
+    email: "proyectodrivego@gmail.com",
+    descripcion:  "Localice nuestras oficinas en el aeropuerto de Cuenca.",
+    horario: {
+      semana: 'Lunes-Viernes: 8AM - 6PM',
+      finDeSemana: "Fines de semana: 8AM - 3PM",
+    },
   },
-  { Ciudad: 'Ambato Centro-Av.Cevallos y Unidad Nacional', 
+  { Ciudad: 'Ambato Centro',
     imagen:'/ambato.jpg', 
-    direccion: 'Av.Cevallos diagonal Av.Unidad Nacional',
-    Contactos: 'Call center: (593) 0939821964'
+    direccion: 'Av.Las Americas y Colombia',
+    Contactos: 'Call center: (593) 0939821964',
+    email: "proyectodrivego@gmail.com",
+    descripcion:  "Localice nuestras oficinas en el terminal terrestre norte.",
+    horario: {
+      semana: 'Lunes-Viernes: 8AM - 6PM',
+      finDeSemana: "Fines de semana: 8AM - 6PM",
+    }
   },
 ]
 
@@ -31,7 +55,13 @@ const Lugares = () =>{
           <img src={oficina.imagen} alt={`Vista de ${location.city}`}/>
           <p>{oficina.direccion}</p>
           <p>{oficina.Contactos}</p>
-          <button onClick={() => navigate(`/lugares/${oficina.Ciudad}`)}> Más Información </button>
+          <p>{oficina.email}</p>
+          <p>{oficina.descripcion}</p>
+          <div className="lugaresCard-horario">
+              <p>Horario de atención</p>
+              <p>{oficina.horario.semana}</p>
+              <p>{oficina.horario.finDeSemana}</p>
+          </div>
         </div>
       ))}
     </div>
